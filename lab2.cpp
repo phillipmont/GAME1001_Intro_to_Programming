3605059188c33d887dc78aeea9e5451bb3547091
phillipmont <phillip.monteiro5@gmail.com>
2023-01-19 10:56:01 -0500

    lab 2 excersize example with Connor added

--- lab2.cpp
+++ lab2.cpp
@@ -2,6 +2,31 @@
 
 int main()
 {
-std::cout << "Hello world!" << std::endl;
+	double number1 = 0.0;
+	double number2 = 0.0;
+	double number3 = 0.0;
+
+	std::cout << "enter 3 numbers." << std::endl;
+
+	std::cout << "Number 1: " << std::endl;
+	std::cin >> number1;
+	std::cout << "You entered: " << number1 << " for number 1" << std::endl << std::endl;
+
+	std::cout << "Number 2: " << std::endl;
+	std::cin >> number2;
+	std::cout << "You entered: " << number2 << " for number 2" << std::endl << std::endl;
+
+	std::cout << "Number 3: " << std::endl;
+	std::cin >> number3;
+	std::cout << "You entered: " << number3 << " for number 3" << std::endl << std::endl;
+
+	double sum = number1 + number2 + number3;
+	double average = sum / 3.0;
+	double product = number1 * number2 * number3;
+
+	std::cout << "Sum: " << sum << std::endl;
+	std::cout << "Average: " << average << std::endl;
+	std::cout << "Product: " << product << std::endl;
+
 	return 0;
 }
