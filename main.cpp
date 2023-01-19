bfc0eaeb312f720737a0f152ec558a06023309f9
phillipmont <phillip.monteiro5@gmail.com>
2023-01-19 10:20:29 -0500

    Added number and math function

--- main.cpp
+++ main.cpp
@@ -8,6 +8,15 @@ int main()
 	std::cout << "Your initial character was: " << character << std::endl;
 	character = character + 1;
 	std::cout << "Your final character was: " << character << std::endl;
+	
+	std::cout << "Enter a number" << std::endl;
+	int number;
+	std::cin >> number;
+	std::cout << "Initial number: " << number << std::endl;
+	std::cout << "Initial number + 2: " << number + 2 << std::endl;
+	std::cout << "Initial number - 2: " << number - 2 << std::endl;
+	std::cout << "Initial number * 2: " << number * 2 << std::endl;
+	std::cout << "Initial number / 2: " << number / 2 << std::endl;
 	return 0;
 }
 
