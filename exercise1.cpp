52aeedffa078bfa43d390fd56aede0200c692f4b
phillipmont <phillip.monteiro5@gmail.com>
2023-01-19 21:00:15 -0500

    added outputs and adjusted spacing

--- exercise1.cpp
+++ exercise1.cpp
@@ -2,6 +2,7 @@
 
 int main()
 {
+	//Declare and initialize variables
 	int muffinSold = 0;
 	int donutSold = 0;
 	float muffinPrice = 0.0f;
@@ -11,25 +12,38 @@ int main()
 	int totalSold = 0;
 	float totalSales = 0.0f;
 
-	std::cout << "Enter muffin price" << std::endl;
+	//Input from user
+	std::cout << "Enter muffin price ($):" << std::endl;
 	std::cin >> muffinPrice;
 
-	std::cout << "Enter number of muffins sold" << std::endl;
+	std::cout << "Enter number of muffins sold:" << std::endl;
 	std::cin >> muffinSold;
 
-	std::cout << "Enter donut price" << std::endl;
+	std::cout << "Enter donut price ($):" << std::endl;
 	std::cin >> donutPrice;
 
-	std::cout << "Enter number of donuts sold" << std::endl;
+	std::cout << "Enter number of donuts sold:" << std::endl;
 	std::cin >> donutSold;
 
-	std::cout << "Muffin Price: " << muffinPrice << std::endl;
-	std::cout << "Quantity of muffins sold: " << muffinPrice << std::endl;
-	std::cout << "Donuts price: " << muffinPrice << std::endl;
-	std::cout << "Quantity of donuts sold: " << muffinPrice << std::endl;
-	std::cout << "Muffin sales: $" << muffinPrice << std::endl;
-	std::cout << "Donut sales: $" << muffinPrice << std::endl;
-	std::cout << "A total of " << totalSold << "items were sold." << std::endl;
+	std::cout << std::endl << std::endl;
+	std::cout << "SUMMARY" << std::endl;
+
+	//Calculations
+	muffinSales = muffinPrice * muffinSold;
+	donutSales = donutPrice * donutSold;
+	totalSold = muffinSold + donutSold;
+	totalSales = muffinSales + donutSales;
+
+
+	//Outputs
+	std::cout << "Muffin Price: $" << muffinPrice << std::endl;
+	std::cout << "Quantity of muffins sold: " << muffinSold << std::endl;
+	std::cout << "Donut price: $" << donutPrice << std::endl;
+	std::cout << "Quantity of donuts sold: " << donutSold << std::endl;
+	std::cout << "Muffin sales: $" << muffinSales << std::endl;
+	std::cout << "Donut sales: $" << donutSales << std::endl;
+	std::cout << "A total of " << totalSold << " items were sold." << std::endl;
 	std::cout << "$" << totalSales << " was collected in sales." << std::endl;
 
+	return 0;
 }
